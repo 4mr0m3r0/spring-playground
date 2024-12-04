07ca153260b054e526febc0dd71c9212d8383bb2
Angel <4mr0m3r0@gmail.com>
2024-12-04 17:48:02 -0300

    Properties

--- src/main/java/org/playground/spring/rest/DemoRestController.java
+++ src/main/java/org/playground/spring/rest/DemoRestController.java
@@ -1,10 +1,23 @@
 package org.playground.spring.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoRestController {
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return "Coach: " + coachName + ", Team name: " + teamName;
+    }
+
     @GetMapping("/")
     public String sayHello() {
         return "Hello World!";
