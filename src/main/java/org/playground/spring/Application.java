37d232401bd96b47261e5220ba57009f276459c0
Angel <4mr0m3r0@gmail.com>
2025-01-08 13:18:24 -0300

    Qualifiers and Redis setup

--- src/main/java/org/playground/spring/Application.java
+++ src/main/java/org/playground/spring/Application.java
@@ -1,7 +1,11 @@
 package org.playground.spring;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 
 /**
  * For scanning packages outside org.playground.spring package, use scanBasePackages.
@@ -10,7 +14,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //		scanBasePackages = {"org.playground.spring", "org.playground.util"}
 //)
 @SpringBootApplication
+//@EnableAutoConfiguration
 public class Application {
+	@Bean
+	public RedisTemplate<?, ?> redisTemplate(RedisConnectionFactory connectionFactory) {
+		RedisTemplate<?, ?> template = new RedisTemplate<>();
+		template.setConnectionFactory(connectionFactory);
+		return template;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
--- src/main/java/org/playground/spring/rest/DemoController.java
+++ src/main/java/org/playground/spring/rest/DemoController.java
@@ -2,6 +2,7 @@ package org.playground.spring.rest;
 
 import org.playground.spring.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +19,7 @@ public class DemoController {
     private Coach myCoach;
 
     @Autowired
-    public void setCoach(Coach coach) {
+    public DemoController(@Qualifier("baseballCoach") Coach coach) {
         this.myCoach = coach;
     }
 
