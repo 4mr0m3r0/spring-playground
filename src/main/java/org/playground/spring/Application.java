94f3ea08dda6be83f681b6f1655a642303373e60
Angel <4mr0m3r0@gmail.com>
2025-03-05 17:58:47 -0300

    @EnableConfigurationProperties

--- src/main/java/org/playground/spring/Application.java
+++ src/main/java/org/playground/spring/Application.java
@@ -1,11 +1,17 @@
 package org.playground.spring;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
 
 /**
  * For scanning packages outside org.playground.spring package, use scanBasePackages.
@@ -15,6 +21,7 @@ import org.springframework.data.redis.core.RedisTemplate;
 //)
 @SpringBootApplication
 //@EnableAutoConfiguration
+@EnableConfigurationProperties
 public class Application {
 	@Bean
 	public RedisTemplate<?, ?> redisTemplate(RedisConnectionFactory connectionFactory) {
@@ -23,8 +30,36 @@ public class Application {
 		return template;
 	}
 
+	@Value("${configuration.projectName}") // Another way to inject properties.
+	void setProjectName(String projectName) {
+		System.out.println("setting project name " + projectName);
+	}
+
+	@Autowired
+	void setEnvironment(Environment environment) { // Another way, using global object.
+		System.out.println("setting environment: " + environment.getProperty("configuration.projectName"));
+	}
+
+	@Autowired
+	void setConfigurationProjectProperties(ConfigurationProjectProperties cp) { // here we're injecting a POJO of properties.
+		System.out.println("configurationProjectProperties: " + cp.getProjectName());
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
 }
+
+@Component
+@ConfigurationProperties("configuration") // it specifies that we want to map all properties that are prefixed with configuration onto POJO properties, on this object.
+class ConfigurationProjectProperties {
+	private String projectName;
+	public String getProjectName() {
+		return projectName;
+	}
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+}
+
