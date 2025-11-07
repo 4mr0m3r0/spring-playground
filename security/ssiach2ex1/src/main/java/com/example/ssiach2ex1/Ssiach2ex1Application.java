2fbbf7b13202074f75612ebc195092ad442e8f96
Angel <angelmanuel.romero@globant.com>
2025-11-07 09:02:29 -0300

    Chapter 2 exercise 1 initial commit

---
+++ security/ssiach2ex1/src/main/java/com/example/ssiach2ex1/Ssiach2ex1Application.java
@@ -0,0 +1,13 @@
+package com.example.ssiach2ex1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ssiach2ex1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ssiach2ex1Application.class, args);
+	}
+
+}
---
+++ security/ssiach2ex1/src/test/java/com/example/ssiach2ex1/Ssiach2ex1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ssiach2ex1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ssiach2ex1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
